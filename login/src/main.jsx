1b322783245b679bf7c06ab015f8780de40499c2
Yash <yashbhardwaj7890@gmail.com>
2024-12-01 16:25:30 +0530

    shift gdg logo

--- login/src/main.jsx
+++ login/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import gdg_logo.png from '/gdg_logo.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
