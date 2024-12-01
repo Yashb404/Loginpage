732f45788cc03aa35d1ad580faf87c5c4b1189a1
Yash <yashbhardwaj7890@gmail.com>
2024-12-01 18:48:38 +0530

    changed to google sign up page colors

--- login/src/Components/Dashboard/Dashboard.jsx
+++ login/src/Components/Dashboard/Dashboard.jsx
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react';
 
 const Dashboard = () => {
   useEffect(() => {
-    window.location.href = 'https://www.google.com';
+    window.location.href = 'https://gdg.community.dev/gdg-on-campus-manipal-university-jaipur-india/';
   }, []);
 
   return (
-    <div>
+    <div style={{ color:'white'}}>
       <h1>Welcome to Dashboard</h1>
-      <p></p>
+      <h1>THIS WILL BE THE LANDING PAGE</h1>
+      <p>Redirects to GDG MUJ WEBSITE FOR NOW</p>
     </div>
   );
 };
