7540385a332a832028911bb5de2594eeb319ea1e
Yash <yashbhardwaj7890@gmail.com>
2024-12-01 16:38:18 +0530

    add gdg logo

--- login/src/main.jsx
+++ login/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import gdg_logo.png from '/gdg_logo.png'
+import gdg_logo from './gdg_logo.png'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
     <BrowserRouter>
+    <img src={gdg_logo} className='gdg_logo'></img>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
