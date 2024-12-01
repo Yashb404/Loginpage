7ae228f76f9b06c4b5a5ebed9feaf1ed233a1cf8
Yash <yashbhardwaj7890@gmail.com>
2024-12-01 15:59:33 +0530

    formhandler setup

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-
-import './App.css'
-import SignupLogin from './Components/SignupLogin/SignupLogin'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SignupLogin from './Components/SignupLogin/SignupLogin';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
-  
-
   return (
-    <>
-      <div>
-        <SignupLogin></SignupLogin>
-      </div>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<SignupLogin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- login/src/Components/SignupLogin/SignupLogin.jsx
+++ login/src/Components/SignupLogin/SignupLogin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SignupLogin.css'
 import mail_icon from '../assets/mail-icon.png'
 import password_icon from '../assets/key-icon.png'
@@ -6,39 +7,93 @@ import user_icon from '../assets/user-icon.png'
 import FormHandler from '../FormHandler';
 
 export const SignupLogin = () => {
-  const [action,setAction] = useState("Sign Up");
+  const navigate = useNavigate();
+  const [action, setAction] = useState("Sign Up");
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+  const [storedData, setStoredData] = useState({ email: "", password: "" });
+
+  const handleLoginSuccess = () => {
+    navigate('/dashboard');
+  };
+
+  const { handleInputChange, handleSubmit } = FormHandler({
+    action,
+    formData,
+    setFormData,
+    setStoredData,
+    storedData,
+    onLoginSuccess: handleLoginSuccess
+  });
   
-    return (
+  return (
     <div className='container'>
-<div className='header'>
-<div className='text'>Sign Up</div>
-<div className='line'></div>
-</div>
-<div className='fields'>
-    {action==="Login"?<div></div>: <div className='input'>
-    <img src="" alt='user_icon'/>
-    <input type="text"/>
-    </div>}
+      <div className='header'>
+        <div className='text'>{action}</div>
+        <div className='line'></div>
+      </div>
+      <div className='fields'>
+        {action === "Login" ? null : (
+          <div className='input'>
+            <img src={user_icon} alt='user_icon'/>
+            <input 
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+              placeholder="Username"
+            />
+          </div>
+        )}
    
+        <div className='input'>
+          <img src={mail_icon} alt='email_icon'/>
+          <input 
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+          />
+        </div>
 
-    <div className='input'>
-    <img src={mail_icon} alt='email_icon'/>
-    <input type="email"/>
-    </div>
-
-    <div className='input'>
-    <img src={password_icon} alt='key_icon'/>
-    <input type="password"/>
-    </div>
-    </div>
-    <div className='submit-box'>
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Signup</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-    </div>
-    <div className="forgotPass"><span>Forgot Password ?</span></div>
+        <div className='input'>
+          <img src={password_icon} alt='key_icon'/>
+          <input 
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            placeholder="Password"
+          />
+        </div>
+      </div>
+      <div className='submit-box'>
+        <div 
+          className={action === "Login" ? "submit gray" : "submit"} 
+          onClick={() => setAction("Sign Up")}
+        >
+          Signup
+        </div>
+        <div 
+          className={action === "Sign Up" ? "submit gray" : "submit"} 
+          onClick={() => setAction("Login")}
+        >
+          Login
+        </div>
+      </div>
+      <div 
+        className="submit" 
+        onClick={handleSubmit}
+      >
+        Submit
+      </div>
+      <div className="forgotPass"><span>Forgot Password ?</span></div>
     </div>
   )
-  
 }
 
 export default SignupLogin;
